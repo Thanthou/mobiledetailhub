cd5f0a5fa2ca8b526210d16ecd3bcb7c6ebd0bed
Thanthou <bcoleman143@gmail.com>
2025-08-21 00:16:52 -0700

    Dev mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { LocationProvider } from './contexts/LocationContext';
 import { AuthProvider } from './contexts/AuthContext';
 import DashboardPage from './pages/affiliateDashboard/DashboardPage';
 import Header from './components/01_header';
-import DevModeDropdown from './components/DevModeDropdown';
 import { useScrollToTop } from './hooks/useScrollToTop';
 
 // Component to handle scroll-to-top functionality
@@ -20,7 +19,6 @@ function App() {
         <Router>
           <ScrollToTop />
           <div>
-            <DevModeDropdown />
             <Routes>
             <Route path="/affiliate-dashboard" element={<DashboardPage />} />
             <Route path="/:businessSlug" element={
